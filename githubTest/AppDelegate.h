d5b506164b0e5d99df42308ba64c1429a74a66f2
xy2jianjia <xy2jianjia@163.com>
2015-06-19 17:10:54 +0800

     我创建了工程..

---
+++ githubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubTest
+//
+//  Created by lanou3g on 15/6/19.
+//  Copyright (c) 2015年 H.Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubTest
+//
+//  Created by lanou3g on 15/6/19.
+//  Copyright (c) 2015年 H.Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
